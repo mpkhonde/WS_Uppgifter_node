70dd1e1955339d4d93f8fde455390af29c9588df
mpkhonde <91606609+mpkhonde@users.noreply.github.com>
2025-08-22 23:54:58 +0200

    Uppgift #4: Express server med ts-node

--- Uppgift 4/index.ts
+++ Uppgift 4/index.ts
@@ -4,7 +4,7 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  res.send('Hello World Välkommen till Uppgifter 4!');
 });
 
 app.listen(port, () => {
